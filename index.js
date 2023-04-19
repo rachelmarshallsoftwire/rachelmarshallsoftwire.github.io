256a0a964dcdc91cbcfb54ca5f7301928899bd14
Rachel <rachel.marshall@softwire.com>
2023-04-19 14:59:22 +0100

    Starting

---
+++ index.js
@@ -0,0 +1,11 @@
+const turtles = ['<img src="beach-turtle.png">', <img src="plant-turtle.png">, <img src="aubergine-turtle.png">, <img src="cocktail-turtle.png">, <img src="surf-turtle.png">]
+
+let index = 0
+
+function getNewTurtle() {
+    const image = document.getElementById("turtle-image")
+    image.innerHTML += turtles[index]
+    index === turtles.length-1 ? index = 0 : index+=1
+    // index+=1
+    console.log(index)
+}
