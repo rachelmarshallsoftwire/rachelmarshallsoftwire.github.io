1a429600ec32268aae7035455ccb053a9a119584
Rachel <rachel.marshall@softwire.com>
2023-04-26 15:30:36 +0100

    Stylin :)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const turtles = ['<img src="beach-turtle.png">', <img src="plant-turtle.png">, <img src="aubergine-turtle.png">, <img src="cocktail-turtle.png">, <img src="surf-turtle.png">]
+const turtles = ['<img src="beach-turtle.png">', '<img src="plant-turtle.png">', '<img src="aubergine-turtle.png">', '<img src="cocktail-turtle.png">', '<img src="surf-turtle.png">']
 
-let index = 0
+let index = 1
 
 function getNewTurtle() {
     const image = document.getElementById("turtle-image")
-    image.innerHTML += turtles[index]
+    image.innerHTML = turtles[index]
     index === turtles.length-1 ? index = 0 : index+=1
     // index+=1
     console.log(index)
